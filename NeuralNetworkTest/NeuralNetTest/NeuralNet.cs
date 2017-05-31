fe5f76f9f7c6b5fe52b53488a3e069c156b17103
Josh <JoshJ.alzate77@gmail.com>
2017-05-31 18:05:21 +0800

    Commit 1

---
+++ NeuralNetworkTest/NeuralNetTest/NeuralNet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetTest
+{
+    public class Layer
+    {
+        List<Neuron> Neurons { get; set; }
+
+        public Layer(int Length)
+        {
+            Neurons = new List<Neuron>(Length);
+        }
+    }
+
+    public class Neuron
+    {
+        List<Dendrite> Dendrites { get; set; }
+
+        public Neuron()
+        {
+
+        }
+    }
+
+    public class Dendrite
+    {
+         
+    }
+}
---
+++ NeuralNetworkTest/NeuralNetTest/Perceptron.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetTest
+{
+    public class Perceptron
+    {
+        List<double> Inputs { get; set; }
+        List<double> Weight { get; set; }
+        CryptoRandom Randomizer;
+
+        public Perceptron()
+        {
+            Init();
+        }
+
+        public Perceptron(List<double> Inputs)
+        {
+            Init();
+
+            foreach (var val in Inputs)
+                AddInput(val);
+        }
+
+        public void Init()
+        {
+            Inputs = new List<double>();
+            Weight = new List<double>();
+            Randomizer = new CryptoRandom();
+        }
+
+        public void AddInput(double value)
+        {
+            Inputs.Add(value);
+            Weight.Add(Randomizer.RandomValue);
+        }
+
+        public void Train()
+        {
+
+        }
+    }
+
+    public class CryptoRandom
+    {
+        public double RandomValue { get; set; }
+
+        public CryptoRandom()
+        {
+            using (RNGCryptoServiceProvider p = new RNGCryptoServiceProvider())
+            {
+                Random r = new Random(p.GetHashCode());
+                this.RandomValue = r.NextDouble();
+            }
+        }
+    }
+}
